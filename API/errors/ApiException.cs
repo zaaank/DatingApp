24640dbbe0f682de8c7ad5953253add835e46e30
Žan Smolič <zan.smolic@netica.si>
2020-12-18 15:17:59 +0100

    vefore restarting dotnet

---
+++ API/errors/ApiException.cs
@@ -0,0 +1,16 @@
+namespace API.errors
+{
+    public class ApiException
+    {
+        public ApiException(int statusCode, string message = null, string detail = null)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Detail = detail;
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Detail { get; set; }
+    }
+}
