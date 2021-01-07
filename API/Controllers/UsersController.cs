a4661a1e93b3e3df20d97546babc4478c90f4d30
Žan Smolič <zan.smolic@netica.si>
2021-01-07 11:14:13 +0100

    end of section 11

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -104,5 +104,28 @@ namespace API.Controllers
             return BadRequest("Failed to set photo!");
         }
 
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(int photoId)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(User.getUsername());
+
+            var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+            if(photo == null) return NotFound();
+
+            if(photo.IsMain) return BadRequest("You cannot delete your main photo");
+
+            if(photo.PublicId != null)
+            {
+                var result = await _photoService.DeletePhotoAsync(photo.PublicId);
+                if(result.Error != null) return BadRequest(result.Error.Message);              
+            }
+            user.Photos.Remove(photo);
+
+            if(await _userRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Something went wrong with deleting the photo");
+        }
+
     }
 }
